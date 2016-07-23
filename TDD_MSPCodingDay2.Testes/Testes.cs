294eee53ebdc9dc33d84a87fb67bce7f6e444079
Renato Groffe <renato.groffe@yahoo.com.br>
2016-07-23 01:17:12 -0300

    Add project files.

---
+++ TDD_MSPCodingDay2.Testes/Testes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_MSPCodingDay2.Testes
+{
+    [TestClass]
+    public class Testes
+    {
+        [TestMethod]
+        public void Teste1()
+        {
+            double valor = CalculoFinanceiro
+                .CalcularValorComJurosCompostos(
+                    10000, 12, 2);
+            Assert.AreEqual(12682.42, valor);
+        }
+
+        [TestMethod]
+        public void Teste2()
+        {
+            double valor = CalculoFinanceiro
+                .CalcularValorComJurosCompostos(
+                    11937.28, 24, 4);
+            Assert.AreEqual(30598.88, valor);
+        }
+
+        [TestMethod]
+        public void Teste3()
+        {
+            double valor = CalculoFinanceiro
+                .CalcularValorComJurosCompostos(
+                    15000, 36, 6);
+            Assert.AreEqual(122208.78, valor);
+        }
+    }
+}
---
+++ TDD_MSPCodingDay2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_MSPCodingDay2
+{
+    public static class CalculoFinanceiro
+    {
+        public static double CalcularValorComJurosCompostos(
+            double valorEmprestimo,
+            int numMeses,
+            double percTaxa)
+        {
+            return Math.Round(valorEmprestimo *
+                Math.Pow(1.0 + (percTaxa / 100.0), numMeses), 2);
+        }
+    }
+}
